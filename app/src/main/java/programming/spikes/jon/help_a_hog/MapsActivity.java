85ae40a2296c1a2bdba8216e954d48f47b46ced8
Jonathan Spikes <jonathan.spikes@wachter.com>
2018-11-11 12:57:21 -0600

    Update to clicked marker background

--- app/src/main/java/programming/spikes/jon/help_a_hog/MapsActivity.java
+++ app/src/main/java/programming/spikes/jon/help_a_hog/MapsActivity.java
@@ -95,7 +95,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startActivity(building);
             }});
 
-        printAllMarkers();
+        //printAllMarkers();
     }
 
 
